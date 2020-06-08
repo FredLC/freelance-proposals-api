e0be6f589a3ae440d8314f59a98dae2d3e279600
fredlc <fredlefevre@outlook.com>
2020-06-08 19:20:34 -0400

    Generated scaffold and seeds data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+10.times do |proposal|
+	Proposal.create!(
+		customer: "Customer #{proposal}",
+		portfolio_url: "http://google.com",
+		tools: "Ruby on Rails, Angular, PostgreSQL",
+		estimated_hours: (80 + proposal),
+		hourly_rate: 120,
+		weeks_to_complete: 12,
+		client_email: "joemambre@test.com"
+	)
+end
